aef4554aaa48f4a0823aecc3d9b341e52f07dfbe
AndiRaihan <dzaky.raihan@gmail.com>
2023-10-29 14:04:35 +0700

    Added partitioning

--- sorting_algo.py
+++ sorting_algo.py
@@ -40,3 +40,50 @@ def merge_sort(array: list[int]) -> list[int]:
             array[k] = right_half[j]
             j += 1
             k += 1
+
+def two_pivot_block_partitioning(array: list[int], start: int, end: int) -> tuple[int, int]:
+    """Returns two pivot index that divide the array into three parts using block partitioning method
+
+    Args:
+        array (list[int]): Array that will be partitioned
+        start (int): The start of the partition
+        end (int): The end of the partition
+
+    Returns:
+        tuple[int, int]: A tuple of the first and second pivot index
+    """
+    BLOCK_SIZE = 1024
+    if (end <= start):
+        return (start, start)
+    pivot1 = array[start]
+    pivot2 = array[end]
+    
+    if (pivot1 > pivot2):
+        array[start], array[end] = array[end], array[start]
+        pivot1, pivot2 = pivot2, pivot1
+    
+    block = [None] * BLOCK_SIZE
+    
+    i, j, k = 1, 1, 1
+    nump, numq = 0, 0
+    
+    while k < end:
+        t = min(BLOCK_SIZE, end - k)
+        for c in range(t):
+            block[numq] = c
+            numq = numq + (pivot2 >= array[k + c])
+        for c in range(numq):
+            array[j+c], array[k+block[c]] = array[k+block[c]], array[j+c]
+        k += t
+        for c in range(numq):
+            block[nump] = c
+            nump += (pivot1 > array[j + c])
+        for c in range(nump):
+            array[i], array[j + block[c]] = array[j + block[c]], array[i]
+            i += 1
+        j += numq
+        nump, numq = 0, 0
+    
+    array[i-1], array[0] = array[0], array[i-1]
+    array[j], array[end] = array[end], array[j]
+    return (i-1, j)
--- test_sorting_algo.py
+++ test_sorting_algo.py
@@ -1,5 +1,5 @@
 import unittest
-from sorting_algo import merge_sort
+from sorting_algo import merge_sort, two_pivot_block_partitioning
 
 
 class TestSortingAlgo(unittest.TestCase):
@@ -34,6 +34,37 @@ class TestSortingAlgo(unittest.TestCase):
         expected_output5 = []
         merge_sort(arr5)
         self.assertEqual((arr5), expected_output5)
+    
+    def test_two_pivot_block_partitioning(self):
+            # Test case 1
+        arr1 = [5, 2, 8, 4, 7, 1, 3]
+        expected_output1 = (2, 4)
+        output1 = two_pivot_block_partitioning(arr1, 0, len(arr1) - 1)
+        self.assertEqual(output1, expected_output1)
+
+        # Test case 2
+        arr2 = [1, 2, 3, 4, 5]
+        expected_output2 = (0, 4)
+        output2 = two_pivot_block_partitioning(arr2, 0, len(arr2) - 1)
+        self.assertEqual(output2, expected_output2)
+
+        # Test case 3
+        arr3 = [5, 4, 3, 2, 1]
+        expected_output3 = (0, 4)
+        output3 = two_pivot_block_partitioning(arr3, 0, len(arr3) - 1)
+        self.assertEqual(output3, expected_output3)
+
+        # Test case 4
+        arr4 = [1]
+        expected_output4 = (0, 0)
+        output4 = two_pivot_block_partitioning(arr4, 0, len(arr4) - 1)
+        self.assertEqual(output4, expected_output4)
+
+        # Test case 5
+        arr5 = []
+        expected_output5 = (0, 0)
+        output5 = two_pivot_block_partitioning(arr5, 0, len(arr5) - 1)
+        self.assertEqual(output5, expected_output5)
 
 
 if __name__ == '__main__':
