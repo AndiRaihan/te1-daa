cca496bf11c64df6735e385b93c748d9ef2bd344
AndiRaihan <dzaky.raihan@gmail.com>
2023-10-30 12:51:02 +0700

    added comments

--- SortingAlgo.java
+++ SortingAlgo.java
@@ -2,6 +2,11 @@ import java.util.Arrays;
 
 public class SortingAlgo {
 
+    /**
+     * Sort a given array of int using merge sort algorithm
+     * Source: <a href="https://www.programiz.com/dsa/merge-sort">programiz</a>
+     * @param array An array of int that want to be sorted
+     */
     public static void mergeSort(int[] array) {
         if (array.length > 1) {
             int mid = array.length / 2;
@@ -37,6 +42,16 @@ public class SortingAlgo {
         }
     }
 
+    /**
+     * Partition an array from start to end into three part using two pivots.
+     * Uses lomuto with block partitioning method.
+     * @param array The array that will be partitioned
+     * @param start The start index of the subarray
+     * @param end The end index of the subarray
+     * @param blockSize The size of the block that will be used
+     * @return An array with index 0 as the first pivot and index 1 as the
+     * second pivot
+     */
     public static int[] twoPivotBlockPartitioning(int[] array, int start, int end, int blockSize) {
         if (end <= start) {
             return new int[]{start, start};
@@ -108,10 +123,16 @@ public class SortingAlgo {
             return;
         }
 
+        // Berdasarkan paper rujukan, untuk data ukuran 2^26, block size yang
+        // optimal adalah 2^10. saya simpulkan bahwa untuk data ukuran 2^n,
+        // block size yang optimal adalah 2^(floor(n/3) + 2)
         int blockSize = (int) Math.pow(2, Math.floor(Math.log(array.length) / Math.log(2) / 3 + 2));
         int[][] stack = new int[array.length][2];
         int top = -1;
 
+        // Quicksort dimodifikasi hingga dalam bentuk iteratif (tapi
+        // behaviornya tetap sama seperti recursive call) karena untuk data
+        // yang terlalu besar, terjadi stack overflow
         stack[++top] = new int[]{start, end};
 
         while (top >= 0) {
