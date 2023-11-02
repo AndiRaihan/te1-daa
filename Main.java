6fb5a77524fde9ec60ac544bdb5d233a97c55eb7
AndiRaihan <dzaky.raihan@gmail.com>
2023-11-02 20:34:21 +0700

    Made easier to copas data to excel

--- Main.java
+++ Main.java
@@ -15,48 +15,56 @@ public class Main {
 
     public static void main(String[] args) {
         Map<String, int[]> data = loadDataset();
-        Map<String, String> memoryAllocationResults = new HashMap<>();
-        Map<String, Double> runningTimeResults = new HashMap<>();
 
         for (int i = 1; i <= 5; i++) {
             try (FileWriter writer = new FileWriter("res/iter" + i + ".txt")) {
-                for (String fileName : fileNames) {
-                    runningTimeResults.put("merge sort: " + fileName,
-                            profileRunningTime(data, fileName, "merge sort"));
-                    writer.write("merge sort: " + fileName + " = " + runningTimeResults.get("merge sort: " + fileName) + " ms\n");
-                    System.out.println("merge sort: " + fileName + " = " + runningTimeResults.get("merge sort: " + fileName) + " ms");
-                }
-                writer.write("=".repeat(100) + "\n");
-                System.out.println("=".repeat(60) + "iter " + i + "=".repeat(60));
+                try (FileWriter writerPureData = new FileWriter("res/iter_pure_data" + i + ".txt")){
+                    for (String fileName : fileNames) {
+                        double res = (profileRunningTime(data, fileName, "merge sort"));
+                        writer.write("merge sort: " + fileName + " = " + res + " ms\n");
+                        writerPureData.write(res + "\n");
+                        System.out.println("merge sort: " + fileName + " = " + res + " ms");
+                    }
+                    writer.write("=".repeat(100) + "\n");
+                    writerPureData.write("=".repeat(100) + "\n");
+                    System.out.println("=".repeat(60) + "iter " + i + "=".repeat(60));
 
-                data = loadDataset();
-                for (String fileName : fileNames) {
-                    String res = profileMemory(data, fileName, "merge sort");
-                    memoryAllocationResults.put("merge sort: " + fileName, res);
-                    writer.write("merge sort: " + fileName + " = " + res + "\n");
-                    System.out.println("merge sort: " + fileName + " = " + res);
-                }
-                writer.write("=".repeat(100) + "\n");
-                System.out.println("=".repeat(60) + "iter " + i + "=".repeat(60));
+                    data = loadDataset();
+                    for (String fileName : fileNames) {
+                        String res = profileMemory(data, fileName, "merge sort");
+                        writer.write("merge sort: " + fileName + " = " + res + "\n");
+                        writerPureData.write(res + "\n");
+                        System.out.println("merge sort: " + fileName + " = " + res);
+                    }
+                    writer.write("=".repeat(100) + "\n");
+                    writerPureData.write("=".repeat(100) + "\n");
+                    System.out.println("=".repeat(60) + "iter " + i + "=".repeat(60));
 
-                data = loadDataset();
-                for (String fileName : fileNames) {
-                    runningTimeResults.put("two pivot block quick sort: " + fileName, profileRunningTime(data, fileName, "two pivot block quick sort"));
-                    writer.write("two pivot block quick sort: " + fileName + " = " + runningTimeResults.get("two pivot block quick sort: " + fileName) + " ms\n");
-                    System.out.println("two pivot block quick sort: " + fileName + " = " + runningTimeResults.get("two pivot block quick sort: " + fileName) + " ms");
-                }
-                writer.write("=".repeat(100) + "\n");
-                System.out.println("=".repeat(60) + "iter " + i + "=".repeat(60));
+                    data = loadDataset();
+                    for (String fileName : fileNames) {
+                        double res = (profileRunningTime(data, fileName, "two pivot block quick sort"));
+                        writer.write("two pivot block quick sort: " + fileName + " = " + res + " ms\n");
+                        writerPureData.write(res + "\n");
+                        System.out.println("two pivot block quick sort: " + fileName + " = " + res + " ms");
+                    }
+                    writer.write("=".repeat(100) + "\n");
+                    writerPureData.write("=".repeat(100) + "\n");
+                    System.out.println("=".repeat(60) + "iter " + i + "=".repeat(60));
 
-                data = loadDataset();
-                for (String fileName : fileNames) {
-                    String res = profileMemory(data, fileName, "two pivot block quick sort");
-                    memoryAllocationResults.put("two pivot block quick sort: " + fileName, res);
-                    writer.write("two pivot block quick sort: " + fileName + " = " + res + "\n");
-                    System.out.println("two pivot block quick sort: " + fileName + " = " + res);
+                    data = loadDataset();
+                    for (String fileName : fileNames) {
+                        String res = profileMemory(data, fileName, "two pivot block quick sort");
+                        writer.write("two pivot block quick sort: " + fileName + " = " + res + "\n");
+                        writerPureData.write(res + "\n");
+                        System.out.println("two pivot block quick sort: " + fileName + " = " + res);
+                    }
+                    writer.write("=".repeat(100) + "\n");
+                    writerPureData.write("=".repeat(100) + "\n");
+                    System.out.println("=".repeat(60) + "iter " + i + "=".repeat(60));
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                writer.write("=".repeat(100) + "\n");
-                System.out.println("=".repeat(60) + "iter " + i + "=".repeat(60));
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -100,7 +108,7 @@ public class Main {
         runSorting(data, fileName, functionType);
         long currentMemory = runtime.totalMemory() - runtime.freeMemory();
 
-        long current = (currentMemory - initialMemory) / 1024;
+        double current = (double) (currentMemory - initialMemory) / 1024;
 
         return "current memory usage: " + current + " KB";
     }
